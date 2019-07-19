400fc6e530f31bd22655964b3fb101e8f56a296b
appintos <apintos@overactive.com>
2019-07-19 13:17:07 -0400

    Added tests.

--- project/tests/test_tbls.py
+++ project/tests/test_tbls.py
@@ -44,7 +44,47 @@ class TestService(BaseTestCase):
             self.assertEqual(response.status_code, 201)
             self.assertIn('REN HOEK', data['data']['attributes']['display_name'])
 
-    def test_persons_get_id(self):
+    def test_persons_tasks_post(self):
+        with self.client:
+            response = self.client.post(
+                '/tasks',
+                data=json.dumps({
+                    'data': {
+                        'type': 'task',
+                        'attributes': {
+                            'what': 'do nothing'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        },
+                        'relationships': {
+                            'tasks': {
+                                'data': [
+                                    {
+                                        'type': 'task',
+                                        'id': '1'
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }), 
+                content_type='application/json',
+            )
+            self.assertEqual(response.status_code, 201)
+
+    def test_persons_id_get(self):
             response = self.client.post(
                 '/persons',
                 data=json.dumps({
@@ -66,7 +106,7 @@ class TestService(BaseTestCase):
             self.assertIn('REN HOEK', data['data']['attributes']['display_name'])
             self.assertIn('1', data['data']['id'])
 
-    def test_persons_patch_id(self):
+    def test_persons_id_patch(self):
             response = self.client.post(
                 '/persons',
                 data=json.dumps({
@@ -99,6 +139,84 @@ class TestService(BaseTestCase):
             self.assertIn('person', data['data']['type'])
             self.assertIn('MARLAND HOEK', data['data']['attributes']['display_name'])
             self.assertIn('1', data['data']['id'])
+
+    def test_persons_id_tasks_patch(self):
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.post(
+                '/tasks',
+                data=json.dumps({
+                    'data': {
+                        'type': 'task',
+                        'attributes': {
+                            'what': 'do nothing'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.patch(
+                '/persons/1',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'id': '1',
+                        'attributes': {
+                            'name': 'Marland Hoek'
+                        },
+                        'relationships': {
+                            'tasks': {
+                                'data': [
+                                    {
+                                        'type': 'task',
+                                        'id': '1'
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('person', data['data']['type'])
+            self.assertIn('MARLAND HOEK', data['data']['attributes']['display_name'])
+            self.assertIn('1', data['data']['id'])
+
+    def test_persons_id_delete(self):
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.delete(f'/persons/1')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Object successfully deleted', data['meta']['message'])
 
     def test_add_task(self):
         with self.client:
