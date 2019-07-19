2029028305d8a447cffb87af0a8b0a80bbb3158c
appintos <apintos@overactive.com>
2019-07-19 11:14:34 -0400

    Added tests.

--- project/tests/test_tbls.py
+++ project/tests/test_tbls.py
@@ -4,7 +4,29 @@ from project.tests.base import BaseTestCase
 
 
 class TestService(BaseTestCase):
-    def test_add_person(self):
+    def test_persons_get(self):
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.get(f'/persons')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('person', data['data'][0]['type'])
+            self.assertIn('REN HOEK', data['data'][0]['attributes']['display_name'])
+            self.assertIn('1', data['data'][0]['id'])
+
+    def test_persons_post(self):
         with self.client:
             response = self.client.post(
                 '/persons',
@@ -21,6 +43,62 @@ class TestService(BaseTestCase):
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 201)
             self.assertIn('REN HOEK', data['data']['attributes']['display_name'])
+
+    def test_persons_get_id(self):
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.get(f'/persons/1')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('person', data['data']['type'])
+            self.assertIn('REN HOEK', data['data']['attributes']['display_name'])
+            self.assertIn('1', data['data']['id'])
+
+    def test_persons_patch_id(self):
+            response = self.client.post(
+                '/persons',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'attributes': {
+                            'name': 'Ren Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            response = self.client.patch(
+                '/persons/1',
+                data=json.dumps({
+                    'data': {
+                        'type': 'person',
+                        'id': '1',
+                        'attributes': {
+                            'name': 'Marland Hoek'
+                        }
+                    }
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('person', data['data']['type'])
+            self.assertIn('MARLAND HOEK', data['data']['attributes']['display_name'])
+            self.assertIn('1', data['data']['id'])
 
     def test_add_task(self):
         with self.client:
